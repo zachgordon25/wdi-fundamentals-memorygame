4ea892640ed890f6f0da1605c201a4ba6197ac70
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-06-02 20:09:04 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,18 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-console.log('User flipped queen');
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log('User flipped king');
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert('You found a match!');
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert ('Sorry, try again.')
+  console.log("Sorry, try again.");
 }
+}
+
+function flipCard (cardId) {
+checkForMatch();
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
