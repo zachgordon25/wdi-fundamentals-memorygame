1b4907654d4180e48a4ac1941cb4923d4e57a30d
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-06-04 21:04:13 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,7 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch () {
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
 } else {
@@ -30,12 +31,23 @@ function checkForMatch () {
 }
 }
 
-function flipCard (cardId) {
+function flipCard () {
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 }
-flipCard(0);
-flipCard(2);
+
 checkForMatch();
+
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
