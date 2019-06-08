2e2f94d6b83ef21e0b1811e790ec14b2ac64d725
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-06-08 16:08:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,8 +33,10 @@ if (cardsInPlay[0] === cardsInPlay[1]){
 
 var flipCard = function () {
 var cardId = this.getAttribute('data-id');
-console.log('User flipped ' + cards[cardId].rank + ' of ' + cards[cardId].suit);
+console.log('User flipped ' + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
+console.log (cards[cardId].suit);
+console.log (cards[cardId].cardImage);
 this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 checkForMatch()
@@ -53,5 +55,4 @@ var createBoard = function () {
 	}
 }
 
-	
 createBoard();
